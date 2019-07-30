67e09fdb827674dd3f69f7eebde036b8a5b18669
John Philip <john.philip@buzzfeed.com>
2019-07-30 14:45:48 -0400

    Got data testing working

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -1,50 +1,55 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import config from '../config';
 
 export default class Data extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            data: [],
-            isLoading: false,
-        };
-    }
-    componentDidMount() {
-        console.log(config);
-        this.setState({ isLoading: true });  
-        fetch('https://api.tasty.co/search/tags', {
-            method: 'GET',
-            headers: {
-                'Access-Control-Allow-Origin': true,
-                'X-AUTH-TOKEN': config.config,
-                'X-Tasty-User-Access-Token': config.config,
-                'Accept': "application/json",
-                "Content-Type": "application/json"
-        },
-    })
 
-    .then(response => response.json())
-    .then(responseJson => {
-        this.setState(
-            { 
-                data: responseJson.results, 
-                isLoading: false 
-            }
-        );
-        console.log(responseJson)
-    })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      isLoading: false,
+    };
+  }
 
-    render() {
-        if (this.state.isLoading) {
-            return <p>Loading ...</p>;
-        }
-        return (
-            <div className="container">
-                <p>Hello: {this.state.data}</p>
-            </div>
-        );
+  async componentDidMount() {
+    this.setState({isLoading: true});
+
+    fetch('http://localhost:5000/api/tags')
+    .then(res => res.json())
+    .then(res => console.log(JSON.parse(res.text)))
+    .catch(err => console.log(err))
+
+    // .then(res => JSON.parse(JSON.stringify(res)));
+
+    // fetch('https://api.tasty.co/search/tags', {
+    //   method: 'GET',
+    //   headers: {
+    //     'X-AUTH-TOKEN': config.config,
+    //     'X-Tasty-User-Access-Token': config.config,
+    //     'Accept': "application/json",
+    //     "Content-Type": "application/json",
+    //     "Fastly": 1
+    //   }
+    // }).then(response => response.json())
+    // .then(responseJson => {
+    //   this.setState({
+    //     data: responseJson.results,
+    //     isLoading: false
+    //   });
+    //   console.log(responseJson);
+    // })
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <p>Loading ...</p>;
     }
+    return (
+      <div className="container">
+        <p>Hello: {this.state.data}</p>
+      </div>
+    );
+  }
 }
 
 
---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const config = require('./config');
+const request = require('superagent');
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/api/tags', async (req, res) => {
+
+  const response = await request.get('https://api.tasty.co/search/tags')
+  .set({
+    'X-AUTH-TOKEN': config.config,
+    'X-Tasty-User-Access-Token': config.config,
+    'Accept': "application/json",
+    "Content-Type": "application/json",
+    "Fastly": 1
+  });
+  res.send(response);
+});
+
+app.listen(5000, () => {
+  console.log('Starting server now.')
+});
