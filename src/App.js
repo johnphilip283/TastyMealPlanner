8fe5bdcabf10494e1763b0a7bab8cf1cd5f92fab
Emily Ji <emilyji349@gmail.com>
2019-07-30 14:42:10 -0400

    small changes to planner cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './pages/Home'
-import Data from './pages/Data'
-// import Test from './pages/Test'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
-          <Route path="/" component={Home}/>
-          <Route path='/data' component={Data}/>
-          {/* <Route path='/test' component={Data}/> */}
+          <Route path="/home" component={Home}/>
         </div>
       </Router>
     </div>
