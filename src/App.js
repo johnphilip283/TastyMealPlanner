eb93698c3b4768e1487cb7cafe83ea9fc1f78741
Emily Ji <emilyji349@gmail.com>
2019-07-30 10:44:56 -0400

    gitignore + fetch get request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './pages/Home'
+import Data from './pages/Data'
+// import Test from './pages/Test'
 
 function App() {
   return (
@@ -8,7 +10,8 @@ function App() {
       <Router>
         <div>
           <Route path="/" component={Home}/>
-          {/* <Route path='/landing' component={Landing}/> */}
+          <Route path='/data' component={Data}/>
+          {/* <Route path='/test' component={Data}/> */}
         </div>
       </Router>
     </div>
---
+++ src/pages/Test.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+export default class Test extends Component {
+    render()
+}
