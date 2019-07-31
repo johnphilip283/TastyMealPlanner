da05701d61c70ddae3b146528c8ab08a77f97e69
John Philip <john.philip@buzzfeed.com>
2019-07-31 10:14:02 -0400

    Refactored code

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './pages/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div>
-          <Route path="/home" component={Home}/>
-        </div>
-      </Router>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  async componentDidMount() {
+    const response = await fetch('http://localhost:5000/api/tags');
+    const res = await response.json();
+    console.log(JSON.parse(res.text));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Route path="/home" component={Home}/>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+
+}
